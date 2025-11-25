854764d2a92081ec5e5d0e33005a5f4e3193b8f8
bhavesh <bhavesh@gestureminds.com>
2025-11-25 12:51:59 +0530

    ENV Changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    apiURL: "http://localhost:8080",
-    // apiURL: "https://verite-be.vercel.app",
+    // apiURL: "http://localhost:8080",
+    apiURL: "https://verite-be.vercel.app",
   },
   images: {
     remotePatterns: [new URL("https://res.cloudinary.com/**")],
